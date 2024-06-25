83fb841bb32dab2149e427903e7e825e789148fd
TomClqncy <jeanbaptistemarzolf@gmail.com>
2024-06-25 14:39:50 +0200

    modif

--- script.py
+++ script.py
@@ -3,23 +3,45 @@ from bs4 import BeautifulSoup
 import json
 import jsonpath_ng as jp
 
-url = 'https://api.camptocamp.org/outings?limit=1'  
+url = 'https://api.camptocamp.org/outings?limit='
+nb_sorties = 30
+url = url + str(nb_sorties)
 response = requests.get(url)
 
 if response.status_code == 200: 
     soup = BeautifulSoup(response.text, 'html.parser')
     page_text = soup.get_text(separator=' ', strip=True)
     json_object = json.loads(page_text)
-    title = jp.parse("$['documents'][0]['locales'][0]['title']")
-    result = title.find(json_object)
-    #print(result[0].value)
-    print(result)
+    
+    for i in range (nb_sorties ) :
+        mon_parsing_title = "$['documents'][" + str(i) + "]['locales'][0]['title']"
+        title = jp.parse(mon_parsing_title)
+        result_title = title.find(json_object)
+        
+        mon_parsing_document_ID = "$['documents'][" + str(i) + "]['document_id']"
+        document_ID = jp.parse(mon_parsing_document_ID)
+        result_document_ID = document_ID.find(json_object)
+        
+        mon_parsing_date = "$['documents'][" + str(i) + "]['date_end']"
+        date = jp.parse(mon_parsing_date)
+        result_date = date.find(json_object)
+        
+        mon_parsing_activite = "$['documents'][" + str(i) + "]['activities']"
+        activite = jp.parse(mon_parsing_activite)
+        result_activite = activite.find(json_object)
+        
+        mon_parsing_cotation = "$['documents'][" + str(i) + "]['global_rating']"
+        cotation = jp.parse(mon_parsing_cotation)
+        result_cotation = cotation.find(json_object)
+        if result_cotation == None :
+            print("erreur")
+        print(result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";"
+              ,result_activite[0].value,";",result_cotation[0].value)
     
     
     #f = open('/Users/jb.marzolf/Downloads/raph/sortieC2C-api.txt','a')
     #f.write(page_text)
     #f.close()
-    #print(json_object['documents'])
     
 else:
     print(f"Erreur {response.status_code}")
