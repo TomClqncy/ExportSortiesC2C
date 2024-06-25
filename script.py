e1da9f2612de687af4de9760cb28798072144a70
TomClqncy <jeanbaptistemarzolf@gmail.com>
2024-06-25 11:01:34 +0200

    Update de Raph

--- script.py
+++ script.py
@@ -1,12 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 import json
 
 url = 'https://api.camptocamp.org/outings?limit=1'  
 response = requests.get(url)
 
-print('Hello World')
-f = open('monFichier.txt',"x")
-
-
-f.write("Hello World")
-f.close
+if response.status_code == 200: 
+    soup = BeautifulSoup(response.text, 'html.parser')
+    page_text = soup.get_text(separator=' ', strip=True)
+    json_object = json.loads(page_text)
+    data = {page_text}
+    pretty_json = json.dumps(data, indent=4)
+    
+    
+    #f = open('/Users/jb.marzolf/Downloads/raph/sortieC2C-api.txt','a')
+    #f.write(page_text)
+    #f.close()
+    print(pretty_json)
+    
+else:
+    print(f"Erreur {response.status_code}")
