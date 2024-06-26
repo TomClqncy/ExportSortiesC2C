122140deaf606bdc0f4be0a0dda53ef47de0489a
TomClqncy <jeanbaptistemarzolf@gmail.com>
2024-06-26 14:51:55 +0200

    modif final

--- script.py
+++ script.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 import json
 import jsonpath_ng as jp
 import ast
+import math
 
-url = 'https://api.camptocamp.org/outings?act=snow_ice_mixed&grat=ED6,ED7'
+url = 'https://api.camptocamp.org/outings?'
 nb_sorties = 30
-#url = url + str(nb_sorties)
 response = requests.get(url)
-
+f = open('/Users/jb.marzolf/Downloads/raph/sortieC2C-api.txt','a')
 if response.status_code == 200: 
     soup = BeautifulSoup(response.text, 'html.parser')
     page_text = soup.get_text(separator=' ', strip=True)
@@ -74,68 +74,46 @@ if response.status_code == 200:
         mon_parsing_geom = "$['documents'][" + str(i) + "]['geometry']['geom']"
         geom = jp.parse(mon_parsing_geom)
         result_geom= geom.find(json_object)
+        result_geom_json = result_geom[0].value        
+        result_geom_str = ''.join(result_geom[0].value)
+        result_geom_json_2 = json.loads(result_geom_str)
+        lat, long = result_geom_json_2['coordinates']
+              
+        lat = (lat * 180) / 20037508.34
+        long = (long * 180) / 20037508.34
+        long = (math.atan(math.pow(math.e, long * (math.pi / 180)))* 360 ) / math.pi -90
         
-        result_geom_json = result_geom[0].value       
-        print("Obj Geom","".join(result_geom[0].value))
-        coordinates = result_geom_json['coordinates']
-        x, y = coordinates
-
-        # Afficher les coordonnées
-        print(f"Coordonnées: x={x}, y={y}")
-    
-        #mon_parsing_coordo = "$['coordinates']"
-        #mon_parsing_coordo = "$['coordinates']"
-        #coordo = jp.parse(mon_parsing_coordo)
-        #result_coordo = coordo.find(result_geom[0].value)
-        
-        #print(result_coordo[0].value)
-                
-        #mon_parsing_lat = "$.coordinates[0]"
-        #mon_parsing_long = "$.coordinates[1]"
-        #lat = jp.parse(mon_parsing_lat)
-        #long = jp.parse(mon_parsing_long)
-        #result_lat = lat.find(result_geom)
-        #result_long = long.find(result_geom[0].value)
-        
-        #print("lat:",str(result_lat))
-        #long = str(result_long)
-        
-        print(result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";"
-              ,result_activite[0].value,";")
-        #print(type(result_geom[0].value))
         act = str(result_activite[0].value)
         
         
         try : 
-            print(result_cotation[0].value)
+            Sortie_str = result_cotation[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat ,"\n" 
         except IndexError:
 
             if act == "['skitouring']":
-                print(result_cotation2[0].value,"/",result_cotation5[0].value)
+                Sortie_str = result_cotation2[0].value,"/",result_cotation5[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
                 
             elif act == "['hiking']":
-                print(result_cotation3[0].value)
+                Sortie_str = result_cotation3[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
                 
             elif act == "['rock_climbing']":
-                print(result_cotation1[0].value)
+                Sortie_str = result_cotation1[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
             
             elif act == "['ice_climbing']":
-                print(result_cotation4[0].value)
+                Sortie_str = result_cotation4[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat ,"\n"
             
             elif act == "['snowshoeing']":
-                print(result_cotation6[0].value)
+                Sortie_str = result_cotation6[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
                 
             elif act == "['via_ferrata']":
-                print(result_cotation7[0].value)
+                Sortie_str = result_cotation7[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
                 
             elif act == "['mountain_biking']":
-                print(result_cotation8[0].value,"/",result_cotation9[0].value)
+                Sortie_str = result_cotation8[0].value,"/",result_cotation9[0].value,";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
             
             else :
-                print("Cotation non rensenseignée")
-                
-        
-
-    #f = open('/Users/jb.marzolf/Downloads/raph/sortieC2C-api.txt','a')
-    #f.write(page_text)
-    #f.close() 
+                Sortie_str = "Cotation non rensenseignée",";",result_title[0].value,";",result_document_ID[0].value,";",result_date[0].value,";",result_activite[0].value,";", long ,";", lat,"\n" 
+            f.write(str(Sortie_str))
+print("effectuer")
+f.close()   
+  
